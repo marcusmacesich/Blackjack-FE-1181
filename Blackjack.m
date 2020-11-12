dcbd020b918851ea5f4ddd999312c21076b754b6
marcusmacesich <marcusmacesich@gmail.com>
2020-11-12 16:57:25 -0500

    Update Blackjack.m

--- Blackjack.m
+++ Blackjack.m
@@ -1,1 +1,14 @@
-my_scene = simpleGameEngine('retro_card.png',16,16,8,[255,255,255]);
+clear
+clc
+
+scene = simpleGameEngine('retro_cards.png',16,16,8,[255,255,255]);
+
+emptySprite = 1;
+blankCardSprite = 2;
+cardSprites = 21:72;
+
+deckValues = [11 2:9 10 10 10 11 2:9 10 10 10 11 2:9 10 10 10 11 2:9 10 10 10]
+% Assigns point values to cards
+
+for i = 1:2
+dealerDraw = randi(
